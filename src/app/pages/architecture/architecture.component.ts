a3deb5f79ab7204649b89e784fd26cbeffefdc7d
cpminipc <cpierres@hotmail.com>
2025-02-04 20:59:46 +0100

    Amélioration look architectures avec Materials

--- src/app/pages/architecture/architecture.component.ts
+++ src/app/pages/architecture/architecture.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  MatAccordion,
+  MatExpansionPanel,
+  MatExpansionPanelHeader,
+  MatExpansionPanelTitle
+} from '@angular/material/expansion';
+import {MatCard, MatCardTitle} from '@angular/material/card';
+import {MatList, MatListItem} from '@angular/material/list';
 
 @Component({
   selector: 'app-architecture',
-  imports: [],
+  imports: [
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelTitle,
+    MatExpansionPanelHeader,
+    MatCard,
+    MatCardTitle,
+    MatList,
+    MatListItem
+  ],
   templateUrl: './architecture.component.html',
   styleUrl: './architecture.component.css'
 })
