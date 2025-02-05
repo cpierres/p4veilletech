66784678662a145f53526b90e8c9c728f91a53f1
cpminipc <cpierres@hotmail.com>
2025-02-05 18:33:46 +0100

    Améliorations : mise en forme et ajout de diagrammes d'architectures

--- src/app/pages/architecture/availability/availability.component.ts
+++ src/app/pages/architecture/availability/availability.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
+import {
+  MatAccordion,
+  MatExpansionPanel,
+  MatExpansionPanelHeader,
+  MatExpansionPanelTitle
+} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-availability',
   imports: [
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelTitle,
+    MatExpansionPanelHeader
   ],
   templateUrl: './availability.component.html',
   styleUrl: './availability.component.css'
