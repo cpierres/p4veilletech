f0b538c5fef5c4cf253ccaaf2735226301988d43
cpminipc <cpierres@hotmail.com>
2025-02-22 16:13:54 +0100

    Ajout de sites de veille pour Angular (experts)

--- src/app/pages/front/front-veille/front-veille.component.ts
+++ src/app/pages/front/front-veille/front-veille.component.ts
@@ -13,7 +13,7 @@ import {MatCard} from '@angular/material/card';
   styleUrl: './front-veille.component.css'
 })
 export class FrontVeilleComponent {
-  sitesVeille: LinkInfo[] = [
+  sitesVeilleAngular: LinkInfo[] = [
     {
       name: 'Angular Officiel',
       link: 'https://angular.io',
@@ -53,6 +53,31 @@ export class FrontVeilleComponent {
       name: 'RxJS (Observable Library)',
       link: 'https://rxjs.dev/',
       title: 'Documentation officielle de RxJS, bibliothèque clé pour Angular'
+    },
+    {
+      name: 'Expert Angular : Alain Chautard',
+      link: 'https://www.angulartraining.com',
+      title: 'Expertise Angular'
+    },
+    {
+      name: 'Expert Angular : Manfred Steyer',
+      link: 'https://www.angulararchitects.io/en/',
+      title: 'Expertise Angular'
+    },
+    {
+      name: 'Expert Angular : Joshua Morony',
+      link: 'https://modernangular.com/',
+      title: 'Expertise Angular'
+    },
+    {
+      name: 'Expert Angular : Joshua Morony (inscription Newsletter)',
+      link: 'https://mobirony.kit.com/4a331b9076',
+      title: 'Expertise Angular'
+    },
+    {
+      name: 'Expert Angular : Simon Dieny - youtube',
+      link: 'https://www.youtube.com/watch?v=1TYrTW7K5is',
+      title: 'Expertise Angular'
     },
   ];
 }
