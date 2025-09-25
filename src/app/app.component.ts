1c76243977cd2a85037b92e1f0439700a78f4fda
pierres <c.pierres@larco.fr>
2025-09-25 21:36:18 +0200

    feat : sidenav dynamique selon taille écran (caché si mobile)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
 import {MatToolbar} from '@angular/material/toolbar';
@@ -6,6 +6,9 @@ import {MatListItem, MatNavList} from '@angular/material/list';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
 import {MatTooltip} from '@angular/material/tooltip';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +17,46 @@ import {MatTooltip} from '@angular/material/tooltip';
   standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild('drawer') drawer!: MatSidenav;
+
   private readonly profileImageDefault: string = 'assets/img/cpierres-photo.png';
   private readonly profileImageAvatar: string = 'assets/img/cpierres-avatar2.png';
   profileImage: string = this.profileImageDefault;
   private readonly tooltipMessageDefault: string = 'Sénior ? Clic pour voir';
   private readonly tooltipMessageAvatar: string = 'non... Dinosaure Reactorus ! (réactif de bout en bout)';
   tooltipMessage: string = this.tooltipMessageDefault;
+
+  private destroy$ = new Subject<void>();
+  isMobile = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    // Détection des appareils mobiles
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small
+    ])
+    .pipe(takeUntil(this.destroy$))
+    .subscribe(result => {
+      this.isMobile = result.matches;
+
+      // Sur mobile, fermer le menu par défaut
+      if (this.isMobile && this.drawer) {
+        this.drawer.close();
+      }
+      // Sur desktop, ouvrir le menu par défaut
+      else if (!this.isMobile && this.drawer) {
+        this.drawer.open();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   toggleImage(): void {
     this.profileImage = this.profileImage === this.profileImageDefault
@@ -45,5 +81,8 @@ export class AppComponent {
         : this.tooltipMessageAvatar;
   }
 
+  toggleMenu() {
+    this.drawer.toggle();
+  }
 }
 
