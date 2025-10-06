b227829bb389dd5c38dbe37f17c9b3ed7d157a9c
cpminipc <cpierres@hotmail.com>
2025-10-06 17:36:12 +0200

    feat : initialiser backend avec IA et Postgresql (+ option vectorielle) avec R2DBC (webflux)

---
+++ backend/src/test/java/com/cpierres/p4veilletech/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpierres.p4veilletech.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
