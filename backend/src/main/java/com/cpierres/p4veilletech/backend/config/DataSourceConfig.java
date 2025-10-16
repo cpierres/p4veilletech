3e06e1d27e25fd071be5406cdf1b46f31fdcdce7
cpminipc <cpierres@hotmail.com>
2025-10-16 23:06:26 +0200

    feat : Mise en place de pgVector (via driver jdbc obligatoire)

---
+++ backend/src/main/java/com/cpierres/p4veilletech/backend/config/DataSourceConfig.java
@@ -0,0 +1,39 @@
+package com.cpierres.p4veilletech.backend.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    public DataSource appDataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password) {
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName("org.postgresql.Driver");
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public JdbcTemplate jdbcTemplate(@Qualifier("appDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
+    }
+}
